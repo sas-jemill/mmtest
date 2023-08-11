b76b16c82ed14299f6737482b533563b4be6bfb3
edmdev <jeff.miller@sas.com>
2023-08-11 14:02:22 +0000

    Commit kaniko files QST /provider/mmtest/sas-jemill-git

---
+++ QST/unzipped/example_test.py
@@ -0,0 +1,17 @@
+import os
+import os.path
+import sys
+
+sys.path.append('/models/resources/viya/18519eb0-f8b3-414e-97da-b3e8650f68df/')
+
+import hmeq_logistic_score
+
+import settings_18519eb0_f8b3_414e_97da_b3e8650f68df
+
+settings_18519eb0_f8b3_414e_97da_b3e8650f68df.pickle_path = '/models/resources/viya/18519eb0-f8b3-414e-97da-b3e8650f68df/'
+
+def score_record(JOB,REASON,CLAGE,CLNO,DEBTINC,DELINQ,DEROG,NINQ,YOJ):
+    "Output: EM_EVENTPROBABILITY,EM_CLASSIFICATION"
+    return hmeq_logistic_score.scoreHMEQLogisticModel(JOB,REASON,CLAGE,CLNO,DEBTINC,DELINQ,DEROG,NINQ,YOJ)
+
+print(score_record("","",151.03,131.68,35.31,97.86,173.03,24.59,38.29))
